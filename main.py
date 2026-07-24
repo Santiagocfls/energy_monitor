0e601986bd38e13ada2f22a868e8df28000e799f
Santiago Cofles <santiago.cofles@utp.edu.co>
2026-07-24 00:38:30 +0000

    Actualizando interfaz y main

--- main.py
+++ main.py
@@ -48,7 +48,6 @@ from sensors.voltage_sensor import VoltageSensor
 from sensors.current_sensor import CurrentSensor
 from data.logger            import DataLogger
 from display.terminal_ui    import TerminalUI
-from display.desktop_ui     import DesktopUI
 from utils.timing           import micros
 
 
@@ -89,7 +88,7 @@ rtc     = RTCSensor()
 dht     = DHTSensor()
 voltage = VoltageSensor(read_adc)
 current = CurrentSensor(read_adc)
-ui      = DesktopUI()
+ui      = TerminalUI()
 
 
 # ============================================================
